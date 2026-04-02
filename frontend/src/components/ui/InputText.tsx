26130157079cb73d1e11d327d6fad2f6b14c8305
Julio <bjulio52@gmail.com>
2026-04-02 06:40:03 -0300

    feat: cria o componente InputText

---
+++ frontend/src/components/ui/InputText.tsx
@@ -0,0 +1,31 @@
+import { Field, Input } from "@chakra-ui/react";
+import InputTextProps from "@/types/InputTextType";
+
+export default function InputText({ placeholder, icon, label }: InputTextProps) {
+  return (
+    <Field.Root>
+      {label && (
+        <Field.Label className="text-sm font-medium text-gray-700">
+          {label}
+        </Field.Label>
+      )}
+
+      <div className="relative flex items-center w-full">
+        {icon && (
+          <span className="absolute left-3 text-gray-400 z-10">
+            {icon}
+          </span>
+        )}
+
+        <Input
+          pl={icon ? "2.5rem" : "1rem"}
+          placeholder={placeholder}
+          size="md"
+          bg="gray.50"
+          borderColor="gray.200"
+          width="full"
+        />
+      </div>
+    </Field.Root>
+  );
+}
